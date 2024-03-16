f3d8f7afcf3d78d7fd9842a4ef7b340fd2a6c1dc
tfutada <tafutada@gmail.com>
2024-03-16 12:56:19 +0900

    add test

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -6,6 +6,7 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import junit.framework.TestCase.assertEquals
 import kotlin.test.Test
+import kotlin.test.assertContains
 
 class ApplicationTest {
     @Test
@@ -18,6 +19,19 @@ class ApplicationTest {
         val response = client.get("/")
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("Hello World!", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+        application {
+            module()
+        }
+
+
+        val response = client.get("/test1")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "Hello From Ktor")
     }
 }
 
