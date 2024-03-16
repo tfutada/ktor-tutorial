0eb1ba36cb9600e1053313d18be3d4efc8703c91
tfutada <tafutada@gmail.com>
2024-03-16 13:47:55 +0900

    global exception handler

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ repositories {
 }
 
 dependencies {
+    // Our new dependency to be added
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
+
+
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,10 +3,19 @@ package com.example.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+    //Add the five lines below
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+    }
+
     routing {
         // Add the line below
         staticResources("/content", "mycontent")
@@ -25,6 +34,11 @@ fun Application.configureRouting() {
             val text = "<h1>Hello From Ktor</h1>"
             val type = ContentType.parse("text/html")
             call.respondText(text, type)
+        }
+
+        //Add the three lines below
+        get("/error-test") {
+            throw IllegalStateException("Too Busy")
         }
     }
 }
