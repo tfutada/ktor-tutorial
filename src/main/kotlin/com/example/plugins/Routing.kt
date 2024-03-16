bd005fd7662553b8099ce45f43bc87b9fdba2a28
tfutada <tafutada@gmail.com>
2024-03-16 12:26:43 +0900

    add new endpoint

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -12,6 +13,13 @@ fun Application.configureRouting() {
 
         post("/abc") {
             call.respondText("Hello World222!")
+        }
+
+        // Add the five lines below
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
         }
     }
 }
